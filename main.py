3fe82864ed49df2ae0cda83b6d4b41b876260c20
Jai Smith <jksmithnyc@gmail.com>
2019-10-22 23:32:15 -0400

    set up echo socket

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 # main.py - DFR Beam
 
+import logging
+
 from flask import Flask
 from flask_sockets import Sockets
 
-import logging
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
 
-@sockets.route('/test')
-def test(ws):
+@sockets.route('/echo')
+def echo_socket(ws):
     while not ws.closed:
         # receive message
         message = ws.receive()
-
-        # check for empty message (disconnected)
         if message is None:
             continue
-
-        logging.debug('Received %s' % message)
-
-if __name__ == '__main__':
-    print("""
-        This can not be run directly because the Flask development server does not
-        support web sockets. Instead, use gunicorn:
-        gunicorn -b 127.0.0.1:8080 -k flask_sockets.worker main:app
-        """)
+        
+        # log message
+        logging.debug(message)
